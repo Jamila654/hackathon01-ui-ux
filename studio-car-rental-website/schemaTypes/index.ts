f0103d8f1689cd5e37c4817d95a66009f9e57260
Jamila654 <136564835+Jamila654@users.noreply.github.com>
2025-01-17 19:15:02 +0500

    updated

--- studio-car-rental-website/schemaTypes/index.ts
+++ studio-car-rental-website/schemaTypes/index.ts
@@ -1,6 +1,6 @@
 import cars from "./cars";
 import shipment from "./shipment";
-import UserOrder from "./UserOrder";
+import UserOrder from "./userOrder";
 
 
 export const schemaTypes = [cars,UserOrder,shipment]
